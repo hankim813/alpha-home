2384b964b630208cf25e0811cd06588a48156531
Han Kim <hankim813@gmail.com>
2014-10-22 08:30:30 -0700

    contact form button data disable with fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,4 @@
-class AdminMailer < ActionMailer::Base
+class ContactMailer < ActionMailer::Base
   default from: "Alpha Home Contact Form Entry <no-reply@alphahome.com>"
   default to: "hankim813@gmail.com"
 end
